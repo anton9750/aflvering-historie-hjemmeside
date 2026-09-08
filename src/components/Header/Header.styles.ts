e6521316739b2c3dda31bff911c08e937c072bf4
Anton <anto0784@elev.techcollege.dk>
2026-09-08 19:38:34 +0200

    i added the right position for the card

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,25 +1,31 @@
 import styled from 'styled-components';
 
-export const HeaderWrapper = styled.div<{ $bgImage: string }>`
+export const HeaderWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 260px;
-  background-image: url(${({ $bgImage }) => $bgImage});
+  background-image: url('/src/assets/header.avif');
   background-size: cover;
-
+  background-position: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Overlay = styled.div`
   position: absolute;
   inset: 0;
- 
+  background: rgba(0, 0, 0, 0.35);
 `;
 
 export const Banner = styled.div`
   position: relative;
   z-index: 1;
   background: ${({ theme }) => theme.bannerBackground};
-
+  border: 1px solid ${({ theme }) => theme.border};
+  padding: 24px 48px;
+  text-align: center;
+  max-width: 480px;
 `;
 
 export const Title = styled.h1`
